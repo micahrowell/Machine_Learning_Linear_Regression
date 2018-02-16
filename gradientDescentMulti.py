0840fdd3c7b25f24c50fd8c2a885499528a33361
Micah <m_r525@txstate.edu>
2018-02-16 15:32:42 -0600

    Finalized multiple variable gradient descent

--- gradientDescentMulti.py
+++ gradientDescentMulti.py
@@ -1,45 +1,30 @@
 from computeCostMulti import *
+
 """
 Perform a single gradient step on the parameter vector theta.
 """
-def gradientDescentMulti(x,y,theta,alpha,num_iters):
+
+
+def gradientDescentMulti(x, y, theta, alpha, num_iters):
     m = len(y)
-    J_history = []
-
+    j_history = []
+    n = len(theta)
     for j in range(num_iters):
+        sums = [0] * n
 
-        sum = [0] * len(theta)
-
-        for k in range(len(theta)):
+        for k in range(n):
             for i in range(m):
                 if k == 0:
-                    sum[k] += hypMulti(x,theta) - y[i]
+                    sums[k] += hypMulti(x[i], theta) - y[i]
                 else:
-                    sum[k] += (hypMulti(x,theta) - y[i]) * x[k-1][i]
+                    sums[k] += (hypMulti(x[i], theta) - y[i]) * x[i][k - 1]
+            sums[k] /= m
+            sums[k] *= alpha
+            sums[k] = theta[k] - sums[k]
 
-        for i in range(len(theta)):
-            sum[i] /= m
+        for p in range(n):
+            theta[p] = sums[p]
 
-        for i in range(len(theta)):
-            theta[i] -= alpha * sum[i]
+        j_history.append(computeCostMulti(x, y, theta))
 
-
-        """
-        sum0 = 0
-        sum1 = 0
-        
-        for i in range(m):
-            sum0 += hyp(x[i], theta) - y[i]
-            sum1 += (hyp(x[i], theta) - y[i]) * x[i]
-
-        sum0 /= m
-        sum1 /= m
-
-        theta[0] = theta[0] - alpha * sum0
-        theta[1] = theta[1] - alpha * sum1
-        """
-
-        J_history.append(computeCostMulti(x, y, theta))
-
-
-    return theta,J_history
+    return theta, j_history
