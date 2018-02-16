cf4598393577cd472c9c3f7fa97227bfec7fa6fe
Micah <m_r525@txstate.edu>
2018-02-16 15:33:07 -0600

    Finalized multiple variable gradient descent

--- ex1_multi.py
+++ ex1_multi.py
@@ -7,7 +7,7 @@ x = []  # this array will hold an array of the features in ex1data2.txt
 y = []  # this array will hold all of the y-values in ex1data2.txt
 
 # I want an array of features where each element is an array of the number of those features, so first I need to
-# grab the first line and establish x[] as such
+# grab the first line and establish x[] as such. This makes it easier to compute mu and sigma.
 firstLine = dataFile.readline()
 firstLine = firstLine.strip('\n')
 firstLine = firstLine.split(',')
@@ -18,22 +18,16 @@ for i in range(len(firstLine)):
     else:
         y.append(float(firstLine[i]))
 
-#print x
-#print y
-
 # for each line in the file, separate the values by the comma, cast them as a float, then append them to their
 # respective arrays
 for line in dataFile:
     line = line.strip('\n')
     line = line.split(',')
     for j in range(len(line)):
-        xEntry = []
         if j != len(line) - 1:
             x[j].append(float(line[j]))
         else:
             y.append(float(line[j]))
-#print x
-#print y
 
 m = len(y)
 
@@ -41,9 +35,9 @@ print 'First 10 examples from the dataset:\n'
 string = 'x = ['
 for i in range(len(x)):
     if i == len(x) - 1:
-        string = string + str(x[i][0:10]) + ']'
+        string += str(x[i][0:10]) + ']'
     else:
-        string = string + str(x[i][0:10]) + ', '
+        string += str(x[i][0:10]) + ', '
 print string
 print 'y = ' + str(y[0:10])
 
@@ -58,13 +52,34 @@ print 'x normalized: ' + str(x)
 print 'mu = ' + str(mu)
 print 'sigma = ' + str(sigma)
 
+# translating x so that it is now an array of rows of data instead of an array of features
+x1 = []
+for j in range(len(x[0])):
+    x1.append([0] * len(x))
+    for i in range(len(x)):
+        x1[j][i] = x[i][j]
+
 print '\nRunning gradient descent . . .'
 alpha = 0.01
 num_iters = 400
 
-theta,J_history = gradientDescentMulti(x,y,[0,0,0], alpha,num_iters)
+theta,J_history = gradientDescentMulti(x1,y,[0,0,0], alpha,num_iters)
 
-# theta = 334302,100387, 3673
+# theta = 334302, 100387, 3673
 
 print 'theta after gradient descent: ' + str(theta)
-print 'J_history: ' + str(J_history)
+
+x_new = [1650,3]
+x_new = [(1650-mu[0])/sigma[0],(3-mu[1])/sigma[1]]
+
+price = 0
+
+for i in range(len(theta)):
+    if i == 0:
+        price += theta[i]
+    else:
+        price += theta[i] * x_new[i-1]
+
+print '\nPredicted price of a 1650 sq-ft, 3 br house (using gradient descent): $' + str(price)
+# print '\nProgram paused. Press enter to continue.'
+# wait = raw_input()
